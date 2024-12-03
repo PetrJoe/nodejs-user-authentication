dba2f1c349483396f7606d2a379bb10f28331a08
Akanmu Akinkunmi <56254171+PetrJoe@users.noreply.github.com>
2024-12-03 17:02:36 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('postgresql://postgres.pqomscjunnbnpmushspv:F5qAWsRUKyCkaeQB@aws-0-eu-central-1.pooler.supabase.com:6543/postgres', {
+const sequelize = new Sequelize('postgresql://postgres:F5qAWsRUKyCkaeQB@aws-0-eu-central-1.pooler:6543/postgres', {
   dialect: 'postgres',
   logging: false,
   pool: {
